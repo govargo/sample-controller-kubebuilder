a34f6f7ad08ae5600a2ab51a65e90425d0f1e794
Iso Kenta <k-iso@colopl.co.jp>
2019-09-03 21:29:46 +0900

    Edit main.go to add import package

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ package main
 import (
 	"flag"
 	"os"
+	"time"
 
 	samplecontrollerv1alpha1 "github.com/govargo/sample-controller-kubebuilder/api/v1alpha1"
 	"github.com/govargo/sample-controller-kubebuilder/controllers"
