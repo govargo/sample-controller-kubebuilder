b71ce94080b9b9b342ccc80afff5dbe32c68e058
Iso Kenta <k-iso@colopl.co.jp>
2019-09-03 21:05:07 +0900

    Edit foo_controller.go to add import package

--- controllers/foo_controller.go
+++ controllers/foo_controller.go
@@ -19,6 +19,11 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
